15433ccc6742ac83b277a644c4f55940c3ed514d
Feguette <liang.j4son@gmail.com>
2016-10-11 10:23:29 -0700

    Update BarGraphDriver.java

--- BarGraphDriver.java
+++ BarGraphDriver.java
@@ -13,11 +13,38 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 public class BarGraphDriver extends JComponent
 {
+    private double[] values;
+    private int barGraphHeight;
+    private int barGraphLength;
+    private int barLength;
+    private double baseLine;
+    private double barRatio;
+    private double valuesUpper;
+    private double valuesLower;
+    
     
     public BarGraphDriver()
     {
+        values[] = {4,-610.5, 2102};
+        barGraphLength = 1000;
         
+        barLength = barGraphLength/values.length;
     }
     
-    
-}
+    public void setterMaxMin()
+    {
+        double varsCur = 0;
+        for(int i = 0; i < values.length; i ++)
+        {
+            valuesUpper = Math.max(varsCur, values[i]);
+            valuesLower = Math.min(varsCur, values[i]);
+        }
+        if (valuesLower > 0)
+        {
+            valuesLower = 0;
+        }
+        if (valuesUpper < 0)
+        {
+            valuesLower = 0;
+        }
+    }
